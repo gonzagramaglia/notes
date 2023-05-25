c0cfc74cba7e90784ef413b2987cf1861d7be4c0
Gonza <gonzagramaglia@gmail.com>
2023-05-25 08:49:38 -0300

    Notes Title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Sidebar = (props) => {
-    const noteElements = props.notes.map((note, index) => (
+    const noteElements = props.notes.map((note) => (
         <div key={note.id}>
             <div
                 
@@ -10,7 +10,7 @@ const Sidebar = (props) => {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{ note.body.split('\n')[0] }</h4>
             </div>
         </div>
     ))
